296c23d3975f33218c68eb036294deea3a3b09a6
Shamon Thaibe <dev01@solutionminds.in>
2024-11-22 13:39:53 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,19 @@ import Form from "./form";
 
 export default function Home() {
   return (
-    <div className="bg-gray-100 h-[100vh] flex justify-center items-center p-6">
+    <div className="bg-gray-100 min-h-screen flex justify-center items-center p-6">
       <div className="bg-white shadow-lg rounded-3xl max-w-7xl w-full flex flex-col md:flex-row gap-8">
         {/* Left Section */}
-        <div className="p-10 md:w-1/2 space-y-8">
-          <h1 className="text-4xl font-extrabold text-blue-900">
+        <div className="p-6 md:p-10 md:w-1/2 space-y-8 min-h-[100vh] flex flex-col justify-center">
+          <h1 className="text-3xl sm:text-4xl font-extrabold text-blue-900">
             Your New Apartment in Dubai Awaits!
           </h1>
-          <p className="text-lg text-blue-600 font-medium">
+          <p className="text-base sm:text-lg text-blue-600 font-medium">
             The luxurious Sidr Residences in the prestigious district of Expo
             City Dubai.
           </p>
-          <p className="text-gray-500 font-light">
-            Fill in the form and our expert will contact you
+          <p className="text-sm sm:text-base text-gray-500 font-light">
+            Fill in the form and our expert will contact you.
           </p>
           <Form />
         </div>
