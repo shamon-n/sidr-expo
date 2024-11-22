0797c757b5e906f74b401b24e165c875a009ef45
Shamon Thaibe <dev01@solutionminds.in>
2024-11-22 14:20:27 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="bg-gray-100 min-h-screen flex justify-center items-center p-6">
       <div className="bg-white shadow-lg rounded-3xl max-w-7xl w-full flex flex-col md:flex-row gap-8">
         {/* Left Section */}
-        <div className="p-6 md:p-10 md:w-1/2 space-y-8 min-h-[100vh] flex flex-col justify-center">
+        <div className="p-6 md:p-10 md:w-1/2 space-y-8 flex flex-col justify-center order-2 md:order-1">
           <h1 className="text-3xl sm:text-4xl font-extrabold text-blue-900">
             Your New Apartment in Dubai Awaits!
           </h1>
@@ -21,8 +21,8 @@ export default function Home() {
           <Form />
         </div>
 
-        {/* Right Section */}
-        <div className="relative md:w-1/2 flex justify-center items-center">
+        {/* Right Section (Image) */}
+        <div className="relative md:w-1/2 flex justify-center items-center order-1 md:order-2">
           <div className="w-[95%] h-[95%] bg-gray-100 rounded-2xl overflow-hidden">
             <Image
               src="https://cdn.uae-flats.com/public/uploads/Whats_App_Image_2024_11_06_at_10_23_55_1_ab689e6784.jpeg"
